43eeb3f69aa4c2346d75793664a6f9298d0802ff
paul52841018 <paul52841018@gmail.com>
2020-12-12 00:28:42 +0800

    New api 7 : finished api1-7 GetUserItems, but need to check  for error test. Now can successfully get user's Item, if no api will return empty object. Api router is: (url)/items/getAllItems

--- app/controller/items.js
+++ app/controller/items.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const items = require('../model/items');
 
 class ItemsController extends Controller {
     async create() {
@@ -44,6 +45,51 @@ class ItemsController extends Controller {
         const _res = await ctx.model.Items.create(itemData)
             .then(() => { ctx.status = 200; ctx.body = "ok"; })
             .catch((err) => { console.log(err); ctx.status = 400; ctx.body = "some error" });
+    }
+    async getAllItems() {
+        const { ctx, app } = this;
+        const userToken = ctx.request.body.userToken;
+        let userData = {};
+        let findedUserID;
+        let findedUserName;
+        jwt.verify(userToken, "my_secret_key", (err, data) => {
+            if (err) { console.log('.........faild retrieve token', err); ctx.status = 400; ctx.body = "usertoken error"; return; }
+            else {
+                userData = Object.assign({}, data['payload']);
+                findedUserName = userData['user_name'];
+                //console.log('payload', data['payload']);
+            }
+        });
+        if (findedUserName != null) {
+            const findedUser = await ctx.model.Users.findOne({ where: { username: findedUserName } });
+            if (findedUser) {
+                console.log('userid is ::', findedUser['dataValues']['id']);
+                findedUserID = findedUser['dataValues']['id'];
+            }
+        }
+        else {
+            ctx.status = 400;
+            ctx.body = "not find the user, contact with backend";
+            console.log('usename is ::::', findedUserName);
+        }
+        const res = await ctx.model.Items.findAll({
+            attributes: ['id', 'name', ['image_url', 'imgURL'], 'price', ['is_display', 'isDisplay']],
+            where: {
+                user_id: findedUserID,
+            }
+        })
+            .then((res) => {
+                ctx.status = 200;
+                console.log('------res1.....', res, 'length: ', res.length);
+                let resData = [];
+                for (let i = 0; i < res.length; i++) {
+                    resData.push(res[i].dataValues);
+                    console.log(i, 'is :', res[i].dataValues);
+                }
+                console.log('finally', resData);
+                ctx.body = resData;
+            })
+            .catch((err) => { ctx.status = 400; ctx.body = '404 for token error'; return; });
     }
 }
 
--- app/router.js
+++ app/router.js
@@ -9,9 +9,11 @@ module.exports = app => {
   router.post('/users/login', controller.session.login);
   router.post('/users/signUp', controller.users.register);
   router.post('/items/new', controller.items.create);
+  router.post('/items/getAllItems', controller.items.getAllItems);
+
   router.get('/sth', controller.home.something);
   router.post('/session/login', controller.session.login);
-  router.post('/session/IsloggedIn',controller.session.IsloggedIn);
-  router.post('/users/getUserData',controller.users.getData);
-  router.post('/session/loginTrigger',controller.session.loginTrigger);
+  router.post('/session/IsloggedIn', controller.session.IsloggedIn);
+  router.post('/users/getUserData', controller.users.getData);
+  router.post('/session/loginTrigger', controller.session.loginTrigger);
 };
