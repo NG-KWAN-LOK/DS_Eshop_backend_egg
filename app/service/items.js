2495a9c2b35730817301c1a772fdd2225554826a
paul52841018 <paul52841018@gmail.com>
2020-12-22 11:56:53 +0800

    Update api change for format

--- app/service/items.js
+++ app/service/items.js
@@ -2,7 +2,8 @@
 
 const Service = require('egg').Service;
 const ErrorRes = require('../lib/errorRes');
-
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 class ItemsService extends Service {
   async createItems(itemData) {
@@ -33,6 +34,77 @@ class ItemsService extends Service {
       })
       .catch(err => { result = err; });
     return result;
+  }
+
+  async searchGoodsbyKeyword(requestData) {
+    const { ctx } = this;
+    let _err = false;
+    console.log('requestData : ', requestData);
+    // const keywords = ctx.request.body.keywords.split(' ');
+    // console.log('requestData : ', requestData.keywords);
+
+    const res = await ctx.model.Items.findAll({
+      attributes: ['id', 'name', 'image_url', 'price', 'remain_quantity', 'sales', 'category'],
+      order: [[requestData.orderByKeyword, requestData.orderBy]],
+      where: {
+        name: { [Op.substring]: requestData.keywords },
+        is_display: true,
+      }
+    }).then(res => {
+      let goodlist = [];
+      for (let i = 0; i < res.length; i++) {
+        goodlist.push({
+          "id": res[i].dataValues.id,
+          "name": res[i].dataValues.name,
+          "imgURL": res[i].dataValues.image_url,
+          "price": res[i].dataValues.price,
+          "sales": res[i].dataValues.sales,
+          "stock": res[i].dataValues.remain_quantity,
+          "category": res[i].dataValues.category,
+        });
+      }
+      return goodlist;
+    })
+      .catch(err => { _err = true; console.log(err); return err; });
+
+    if (_err === true) { return 'err' + res; }
+    else { return res; }
+
+  }
+
+  async searchGoodsWithCategory() {
+    const { ctx } = this;
+    let _err = false;
+    // console.log('key : ', ctx.request.body.keywords);
+    // const keywords = ctx.request.body.keywords.split(' ');
+    const res = await ctx.model.Items.findAll({
+      attributes: ['id', 'name', 'image_url', 'price', 'remain_quantity', 'sales', 'category'],
+      order: [[requestData.orderByKeyword, requestData.orderSeq]],
+      where: {
+        name: { [Op.substring]: requestData.keywords },
+        is_display: true,
+        price: { [Op.between]: [requestData.minPrice, requestData.maxPrice], },
+      }
+    }).then(res => {
+      let goodlist = [];
+      for (let i = 0; i < res.length; i++) {
+        goodlist.push({
+          "id": res[i].dataValues.id,
+          "name": res[i].dataValues.name,
+          "imgURL": res[i].dataValues.image_url,
+          "price": res[i].dataValues.price,
+          "sales": res[i].dataValues.sales,
+          "stock": res[i].dataValues.remain_quantity,
+          "category": res[i].dataValues.category,
+        });
+      }
+      return goodlist;
+    })
+      .catch(err => { _err = true; console.log(err); return err; });
+
+    if (_err === true) { return 'err'; }
+    else { return res; }
+
   }
 
 }
